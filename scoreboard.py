32e931b99a5e57a39326a0beb0d9ccc1f35ee80a
Kirtan Gajjar <w.kgajjar@gmail.com>
2025-05-25 09:54:18 +0530

    Updated game_over() method to create highest score field

--- scoreboard.py
+++ scoreboard.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 from food import Food
 
 ALIGNMENT = "center"
-FONT = ("Courier",25,"normal")
+FONT = ("Courier",20,"bold")
 
 class Score(Turtle):
     def __init__(self):
@@ -12,19 +12,24 @@ class Score(Turtle):
         self.color("white")
         self.hideturtle()
         self.score=0
+        self.highest_score = 0
         self.update_screen()
 
     def update_screen(self):
-        self.write(arg=f"Score : {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score : {self.score} | Highest score : {self.highest_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg="GAME OVER", move=False, align=ALIGNMENT, font=("Courier",28,"bold"))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(arg="GAME OVER", move=False, align=ALIGNMENT, font=("Courier",28,"bold"))
 
+    
+    def reset_score(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+        self.score = 0
+        self.update_screen()
 
     def scoring(self):
         self.score+=1
-        self.clear()
-        self.write(arg=f"Score : {self.score}", move=False, align=ALIGNMENT, font=FONT)
-
-
+        self.update_screen()
